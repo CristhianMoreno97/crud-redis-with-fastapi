73d4ade8ec34744ec44964323aab88df27c345d9
CristhianMoreno97 <cristian_21_97@hotmail.com>
2023-05-23 23:18:50 -0500

    crud - define delete_hash

--- redis_client/crud.py
+++ redis_client/crud.py
@@ -39,3 +39,22 @@ def save_hash(key: str, data: dict):
         redis_client.hset(key, mapping=data)
     except ConnectionError as e:
         raise ConnectionError("Error de conexión con el servidor Redis") from e
+    
+def delete_hash(key: str, keys: list):
+    """
+    Elimina uno o más campos de un hash en el servidor Redis bajo la clave especificada.
+
+    Args:
+        key (str): La clave bajo la cual se encuentra el hash.
+        keys (list): La lista de campos que se eliminarán del hash.
+
+    Raises:
+        ConnectionError: Si ocurre un error de conexión con el servidor Redis.
+    """
+    redis_client = get_redis_client()
+
+    try:
+        redis_client.hdel(key, *keys)
+    except ConnectionError as e:
+        raise ConnectionError("Error de conexión con el servidor Redis") from e
+    
