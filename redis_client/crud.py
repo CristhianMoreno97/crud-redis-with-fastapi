a898281a4b8e823b3e74b48fbc1c55598afa9caf
CristhianMoreno97 <cristian_21_97@hotmail.com>
2023-05-23 23:05:09 -0500

    crud - obtener un hash del servidor Redis bajo la clave especificada.

--- redis_client/crud.py
+++ redis_client/crud.py
@@ -1,6 +1,27 @@
 from .connection import get_redis_client
 from redis.exceptions import ConnectionError
 
+def get_hash(key: str):
+    """
+    Obtiene un hash del servidor Redis bajo la clave especificada.
+
+    Args:
+        key (str): La clave bajo la cual se encuentra el hash.
+
+    Returns:
+        dict: El diccionario de datos del hash.
+
+    Raises:
+        ConnectionError: Si ocurre un error de conexión con el servidor Redis.
+    """
+    redis_client = get_redis_client()
+
+    try:
+        data = redis_client.hgetall(key)
+        return data
+    except ConnectionError as e:
+        raise ConnectionError("Error de conexión con el servidor Redis") from e
+
 def save_hash(key: str, data: dict):
     """
     Guarda un hash en el servidor Redis bajo la clave especificada.
