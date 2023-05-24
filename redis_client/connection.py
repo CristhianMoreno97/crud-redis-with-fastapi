fe9bdee8377cddccbaed2ee0a0cb09106fd203dd
CristhianMoreno97 <cristian_21_97@hotmail.com>
2023-05-23 22:46:13 -0500

    se define archivo para la conexion con redis

---
+++ redis_client/connection.py
@@ -0,0 +1,14 @@
+from redis import Redis
+from os import getenv
+from redis.exceptions import ConnectionError
+
+try:
+    redis_client = Redis(
+        host= getenv('REDIS_HOST'),
+        port= getenv('REDIS_PORT'),
+        password= getenv('REDIS_PASSWORD'),
+        ssl= bool(getenv('REDIS_SSL'))
+    )
+    print("Redis client connected successfully")
+except ConnectionError as e:
+    print(e)
